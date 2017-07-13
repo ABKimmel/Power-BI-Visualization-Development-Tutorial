aa1be5425515c2e423f4345e3f136513c1e184ce
Ben Kimmel <bkimmel@allegient.com>
2017-07-13 14:59:45 -0400

    added table data access example

--- TableDemo/src/visual.ts
+++ TableDemo/src/visual.ts
@@ -29,31 +29,53 @@ module powerbi.extensibility.visual {
     export class Visual implements IVisual {
         private target: HTMLElement;
         private updateCount: number;
-        private settings: VisualSettings;
+        private columnIndices: {};
 
         constructor(options: VisualConstructorOptions) {
-            console.log('Visual constructor', options);
             this.target = options.element;
             this.updateCount = 0;
+            this.columnIndices = [];
+            for (let i = 1; i < 5; i++) {
+                let name = "c" + i;
+                this.columnIndices[name] = 0;
+            }
         }
 
         public update(options: VisualUpdateOptions) {
-            this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
-            console.log('Visual update', options);
-            this.target.innerHTML = `<p>Update count: <em>${(this.updateCount++)}</em></p>`;
+            this.findColumns(options.dataViews[0].metadata.columns);
+            let rows = options.dataViews[0].table.rows;
+
+            let table = "<table> <thead>";
+            table += "<tr> <th>Column One</th> <th>Column Two</th> <th>Column Three</th> <th>Column Four</th> </tr>";
+            table += "</thead> <tbody>";
+            for (let i = 0; i < rows.length; i++) {
+                let row = rows[i];
+                table += this.makeRowHTML(row, this.columnIndices["c1"], this.columnIndices["c2"], this.columnIndices["c3"], this.columnIndices["c4"]);
+            }
+            table += "</tbody> </table>";
+            this.target.innerHTML = table;
         }
 
-        private static parseSettings(dataView: DataView): VisualSettings {
-            return VisualSettings.parse(dataView) as VisualSettings;
+        private makeRowHTML(row: any[], c1Index: number, c2Index: number, c3Index: number, c4Index: number): string {
+            return `<tr><td>${(row[c1Index])}</td> <td>${(row[c2Index])}</td> <td>${(row[c3Index])}</td> <td>${(row[c4Index])}</td></tr>`
         }
 
-        /** 
-         * This function gets called for each of the objects defined in the capabilities files and allows you to select which of the 
-         * objects and properties you want to expose to the users in the property pane.
-         * 
-         */
-        public enumerateObjectInstances(options: EnumerateVisualObjectInstancesOptions): VisualObjectInstance[] | VisualObjectInstanceEnumerationObject {
-            return VisualSettings.enumerateObjectInstances(this.settings || VisualSettings.getDefault(), options);
+        private findColumns(columns: DataViewMetadataColumn[]) {
+            //iterate over defined column names
+            for (let name in this.columnIndices) {
+                //now iterate over available columns, note that not all columns may be assigned a data field yet
+                for (let j = 0; j < columns.length; j++) {
+                    //defining the role attribute of the current column, more info in the data view appendix
+                    let columnRoles = columns[j].roles;
+                    //column name is the property name, so looking in there
+                    if (Object.keys(columnRoles).indexOf(name) >= 0) {
+                        //setting the index of the column name to the index of the role
+                        this.columnIndices[name] = j;
+                        break;
+                    }
+                }
+            }
         }
+
     }
 }
--- docs/appendices/DataExtraction.md
+++ docs/appendices/DataExtraction.md
@@ -21,3 +21,54 @@ public update(options: VisualUpdateOptions) {
 Once you've pulled out the value, you're set!
 
 There is a simple demonstration visualization for the `single` data mappinig available in the [SingleDemo]() folder. If you want to review the concept of a `single` data mapping, check out its [coverage](/docs/appendices/dataViewMappings.md#single) in the [DataView Mappings](/docs/appendices/dataViewMappings.md) appendix.
+
+##Table Mapping
+---
+
+Accessing your data from a `table` mapping can be more difficult than you expect, due to the fact that you are not guaranteed the columns will come back in the order you specify in `capabilities.json`. If you want to determine which columns are which in the data rows, you will have to parse through the `metadata` property of the DataView to determine which column is which. If you just want to access rows, without caring about column identifiers they are simple to access:
+
+```
+public update(options: VisualUpdateOptions) {
+
+    ...
+
+    let rows = options.dataViews[0].table.rows;
+    ...
+
+}
+```
+
+You should also keep in mind that the rows will not be in any particular order unless you have specified a `sorting` property.
+
+If you want to identify the columns of the DataView before accessing the data, you will need to write a function similar to this:
+
+```
+private findColumns(columns: DataViewMetadataColumn[]) {
+    //iterate over defined column names
+    for (let i = 0; i < this.columnIndices.length; i++) {
+        //defining name value from our preconstructed map of names
+        let name = this.columnIndices[i].name;
+        //now iterate over available columns, note that not all columns may be assigned a data field yet
+        for (let j = 0; j < columns.length; j++) {
+            //defining the role attribute of the current column, more info in the data view appendix
+            let columnRoles = columns[j].roles;
+            //column name is the property name, so looking in there
+            if (Object.keys(columnRoles).indexOf(name) >= 0) {
+                //setting the index of the column name to the index of the role
+                this.columnIndices[i].index = j;
+                break;
+            }
+        }
+    }
+}
+```
+
+You will also have to define and initialize the field `columnIndices` as so:
+
+```
+private columnIndices: { "name": string, "index", number}[];
+```
+
+Initialize it by filling in the **internal** names of your data roles.
+
+Once you have the indices of your columns, you can access the row data with certainty about what information you are getting.
